36c139b2d7ed4ced0400a4ebbf65c31b2c126e6d
Vinicius <www.vinnyboy@gmail.com>
2020-05-15 00:33:02 -0300

    Upload palindromic exercise

---
+++ projects/Project Euler/Palindromic.py
@@ -0,0 +1,11 @@
+#Largest palindrome productnumber = 0
+
+for i in range(999,100,-1):
+    for j in range(999,100,-1):
+        a = list(str(i*j))
+        b=a[:]
+        b.reverse()
+        if(a==b and (i*j) > number):
+            number = i*j
+
+print(number)
